82264f06a0b12ebd7d70ce80d8e79bb3f5c76e1c
akr-38 <ayushakr38@gmail.com>
2025-10-11 23:54:09 +0530

    created most of the crud apis, also replaced ts-node with tsx, running fine inside the container

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
